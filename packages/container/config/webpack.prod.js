3f731739f4e4430206b117280e55dabad3758160
Sri Ram Teja Komerisetti <sriram.komerisetti@7-11.com>
2021-02-17 16:55:34 -0600

    added conatiner workflow

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+const domain = process.env.PRODUCTION_DOMAIN
+
+module.exports = {
+  mode: 'production',
+  output: {
+    filename: '[name][contenthash].js',
+  },
+  devServer: {
+    port: 5000,
+    open: true,
+    hot: true,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared: ['react', 'react-dom', 'react-router-dom', '@material-ui/core', '@material-ui/icons'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+}
